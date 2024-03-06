139532ba3e6cdcf05d80bd0eb24c838f773abbe1
randaaz <randaaziz972@gmail.com>
2024-03-05 21:11:58 -0800

    fix the error

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from fabric.api import env, local, put, run, runs_once
 
 
-env.hosts = ['54.86.220.207', '54.175.137.217']
+env.hosts = ['54.157.162.121', '54.174.153.180']
 
 
 @runs_once
