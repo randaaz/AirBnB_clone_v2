1348ed6c440b989b51bd8af8319af5b6480b571f
randaaz <randaaziz972@gmail.com>
2024-03-20 23:38:24 -0700

    Script that starts a Flask web application

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Simple flask app that returns "Hello HBNB!" on the root route.
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Returns:
+        str: 'Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
