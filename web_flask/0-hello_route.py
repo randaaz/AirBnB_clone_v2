661f14b16e72bb379a2f933ec0f53a472b11aea5
randaaz <randaaziz972@gmail.com>
2024-03-22 00:07:27 -0700

    trying fix the error

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
 """
 Simple flask app that returns "Hello HBNB!" on the root route
 """
+
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello():
-    """Returns:
-        str: 'Hello HBNB!"""
+def index():
+    """returns Hello HBNB!"""
     return 'Hello HBNB!'
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
